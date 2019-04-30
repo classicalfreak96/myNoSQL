de013b2b080e8adcf286ca3083d867f87c40e021
HuangDavid <d.huang@wustl.edu>
2019-04-30 03:43:58 -0500

    added Document tests

--- src/test/DocumentTester.java
+++ src/test/DocumentTester.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import hw5.Document;
@@ -25,6 +26,26 @@ class DocumentTester {
 		String json = "{ \"key\": \"value\" }";
 		JsonObject results = Document.parse(json);
 		assertTrue(results.getAsJsonPrimitive("key").getAsString().equals("value"));
+	}
+	
+	@Test
+	public void testParseArrays() {
+		String json = "{ \"numbers\": [{\"type\": \"fax\", \"number\": \"123 456-7890\"}] }";
+		JsonObject results = Document.parse(json);
+		assertTrue(results.getAsJsonPrimitive("numbers").getAsJsonArray().get(0)
+				.getAsJsonObject().get("type").getAsString().equals("fax"));
+		assertTrue(results.getAsJsonPrimitive("numbers").getAsJsonArray().get(0)
+				.getAsJsonObject().get("number").getAsString().equals("123 456-7890"));
+	}
+	
+	@Test
+	public void testParseDoc() {
+		String json = "{ \"numbers\": {\"type\": \"fax\", \"number\": \"123 456-7890\"} }";
+		JsonObject results = Document.parse(json);
+		assertTrue(results.getAsJsonPrimitive("numbers").getAsJsonObject()
+				.get("type").getAsString().equals("fax"));
+		assertTrue(results.getAsJsonPrimitive("numbers").getAsJsonObject()
+				.get("number").getAsString().equals("123 456-7890"));
 	}
 
 }
