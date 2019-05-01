83e768cc28c58d0e25a3a32117b19252a4712f7e
Harrison Lu <harrisonlu@wustl.edu>
2019-05-01 13:30:14 -0500

    began work on cursor, works when returning all

--- src/hw5/DBCursor.java
+++ src/hw5/DBCursor.java
@@ -5,30 +5,44 @@ import java.util.Iterator;
 import com.google.gson.JsonObject;
 
 public class DBCursor implements Iterator<JsonObject>{
-
+	
+	DBCollection result;
+	long count;
+	int current = 0;
+	
+	
 	public DBCursor(DBCollection collection, JsonObject query, JsonObject fields) {
-		
+		//find all
+		if (query == null && fields == null) {
+			result = collection;
+		}
+		this.count = this.count();
 	}
 	
 	/**
 	 * Returns true if there are more documents to be seen
 	 */
 	public boolean hasNext() {
-		return false;
+		return current == count ? false : true;
 	}
 
 	/**
 	 * Returns the next document
 	 */
 	public JsonObject next() {
-		return null;
+		if (this.hasNext()) {
+			this.current ++;
+			return this.result.getDocument(this.current - 1);
+		}
+		else return null;
 	}
+
 	
 	/**
 	 * Returns the total number of documents
 	 */
 	public long count() {
-		return 0;
+		return this.result.count();
 	}
 
 }
