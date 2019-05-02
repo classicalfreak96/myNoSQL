3aeb91459a6686f8f9176a947f0265b783fef57a
Harrison Lu <harrisonlu@wustl.edu>
2019-05-02 00:18:52 -0500

    approaching cursor in different way

--- src/hw5/DBCursor.java
+++ src/hw5/DBCursor.java
@@ -12,57 +12,31 @@ import com.google.gson.JsonObject;
 
 public class DBCursor implements Iterator<JsonObject>{
 	
-	DBCollection result;
+//	DBCollection result;
 	long count;
 	int current = 0;
-	
+	ArrayList<JsonObject> result = new ArrayList<>();
 	
 	public DBCursor(DBCollection collection, JsonObject query, JsonObject fields) throws Exception {
+		
 		//find all
-		this.result = collection;
+//		this.result = collection;
 		
 //		JsonObject toReturn = new JsonObject();
-//		for (long i = 0; i < collection.count(); i++) {
-//			JsonObject toAdd = collection.getDocument(i);
-//			for (String key : toAdd.keySet()) {
-//				toReturn.add(key, toAdd.get(key));
-//			}
-//		}
+		for (long i = 0; i < collection.count(); i++) {
+			result.add(collection.getDocument(i));
+		}
 		
 		//handle the query
 		if (query != null) {
-			this.result = collection;
-//			Set<Entry<String, JsonElement>> querySet = query.entrySet();
-//			ArrayList<String> toRemove = new ArrayList<>();
-//			for (long i = 0; i < this.result.count(); i++) {									  									//for each row
-//				HashMap<String, JsonElement> document = new Gson().fromJson(this.result.getDocument(i).toString(), HashMap.class);	//row to hashmap for better access time
-//				for (Entry<String, JsonElement> entry : querySet) {				//for each query
-//					if (document.get(entry.getKey()) != entry.getValue()) {
-//						toRemove.add(entry.getKey());
-//					}
-//				}
-//			}
-			
-//			ArrayList<String> toRemove = new ArrayList<>();
-//			Set<String> queryKeys= query.keySet();
-//			for (long i = 0; i < this.result.count(); i++) { //iterate through rows in results to compare with query 
-//				JsonObject row = this.result.getDocument(i);
-//				for (String key : queryKeys) {
-//					JsonElement element = row.get(key);
-//					if (element != query.get(key)) {
-//						toRemove.add(row.get(key).toString());
-//						this.result.drop();
-//					}
-//				}
-//				
-//			}
+
 			
 		}
 		//handle the projection 
 		if (fields != null) {
 			
 		}
-		this.count = this.count();
+		this.count = this.result.size();
 	}
 	
 	/**
@@ -78,7 +52,8 @@ public class DBCursor implements Iterator<JsonObject>{
 	public JsonObject next() {
 		if (this.hasNext()) {
 			this.current ++;
-			return this.result.getDocument(this.current - 1);
+//			return this.result.getDocument(this.current - 1);
+			return this.result.get(this.current - 1);
 		}
 		else return null;
 	}
@@ -88,7 +63,7 @@ public class DBCursor implements Iterator<JsonObject>{
 	 * Returns the total number of documents
 	 */
 	public long count() {
-		return this.result.count();
+		return this.result.size();
 	}
 
 }
